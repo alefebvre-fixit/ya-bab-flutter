fd7a52469ccbeb6a4090d3fe90bf8ee2774f604b
Antoine Lefebvre <antoinelefebvre@gmail.com>
2018-03-03 11:59:46 -0800

    initial commit

---
+++ lib/group.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart'; // new
+import 'dart:async'; // new
+import 'package:firebase_auth/firebase_auth.dart'; // new
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final googleSignIn = new GoogleSignIn();
+final auth = FirebaseAuth.instance; // new
+
+final reference = Firestore.instance.collection('groups');
+
+
+class Group {
+
+  final String name;
+  final String id;
+
+
+  Group({this.id, this.name});
+
+  factory Group.fromDocument(DocumentSnapshot json) {
+    return new Group(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+}
+
+class GroupWidget extends StatelessWidget {
+
+  Group group;
+
+  GroupWidget(Group group) {
+    this.group = group;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Group" + group.name),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new RaisedButton(onPressed: () {
+              Navigator.of(context).pop(true);
+            }, child: new Text("Back to Screen 1"),)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GroupCard extends StatelessWidget {
+
+  Group group;
+
+  GroupCard(Group group) {
+    this.group = group;
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new ListTile(
+            leading: const Icon(Icons.recent_actors),
+            title: new Text(group.name),
+            subtitle: const Text(
+                'Music by Julie Gable. Lyrics by Sidney Stein.'),
+          ),
+          new ButtonTheme
+              .bar( // make buttons use the appropriate styles for cards
+            child: new ButtonBar(
+              children: <Widget>[
+                new FlatButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {
+                    /* ... */
+                  },
+                ),
+                new FlatButton(
+                  child: const Text('OPEN'),
+                  onPressed: () {
+
+                    Navigator.of(context).push(new MaterialPageRoute<Null>(
+                      builder: (BuildContext context) {
+                        return new GroupWidget(
+                          this.group
+                        );
+                      },
+                    ));
+
+                    /* ... */
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class GroupListTile extends StatelessWidget {
+
+  Group group;
+
+  GroupListTile(Group group) {
+    this.group = group;
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      title: new Text(this.group.name),
+      subtitle: new Text(this.group.name + "-from obj"),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';                   // new
+import 'dart:async';                                               // new
+import 'package:firebase_auth/firebase_auth.dart';                // new
+import 'package:firebase_database/firebase_database.dart';         //new
+import 'package:firebase_database/ui/firebase_animated_list.dart'; //new
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:yabab/group.dart';
+
+
+final googleSignIn = new GoogleSignIn();
+final auth = FirebaseAuth.instance;                              // new
+
+final reference = Firestore.instance.collection('groups');
+
+
+
+
+
+void main() => runApp(new MyApp());
+
+Future<Null> _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null)
+    user = await googleSignIn.signInSilently();
+  if (user == null) {
+    await googleSignIn.signIn();
+  }
+  if (await auth.currentUser() == null) {                          //new
+    GoogleSignInAuthentication credentials =                       //new
+    await googleSignIn.currentUser.authentication;                 //new
+    await auth.signInWithGoogle(                                   //new
+      idToken: credentials.idToken,                                //new
+      accessToken: credentials.accessToken,                        //new
+    );                                                             //new
+  }                                                               //new
+}
+
+class BookList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new StreamBuilder(
+      stream: Firestore.instance.collection('groups').snapshots,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return new Text('Loading...');
+        return new ListView(
+          children: snapshot.data.documents.map((document) {
+            return new Container(
+              padding: new EdgeInsets.all(20.0),
+                child:new GroupCard(
+                  new Group.fromDocument(document)
+              )
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
+
+
+
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: <String, WidgetBuilder> { //5
+        '/screen1': (BuildContext context) => new MyHomePage(title: 'Flutter Demo Home Page'), //6
+        '/screen2' : (BuildContext context) => new GroupWidget() //7
+      },
+    );
+  }
+}
+
+
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  Future _incrementCounter() async {
+
+    await _ensureLoggedIn();
+
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new BookList(),
+
+      
+//      body: new Center(
+//        // Center is a layout widget. It takes a single child and positions it
+//        // in the middle of the parent.
+//        child: new Column(
+//          // Column is also layout widget. It takes a list of children and
+//          // arranges them vertically. By default, it sizes itself to fit its
+//          // children horizontally, and tries to be as tall as its parent.
+//          //
+//          // Invoke "debug paint" (press "p" in the console where you ran
+//          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+//          // window in IntelliJ) to see the wireframe for each widget.
+//          //
+//          // Column has various properties to control how it sizes itself and
+//          // how it positions its children. Here we use mainAxisAlignment to
+//          // center the children vertically; the main axis here is the vertical
+//          // axis because Columns are vertical (the cross axis would be
+//          // horizontal).
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            new Text(
+//              'You have pushed the button this many times:',
+//            ),
+//            new Text(
+//              '$_counter',
+//              style: Theme.of(context).textTheme.display1,
+//            ),
+//          ],
+//        ),
+//      ),
+      floatingActionButton: new FloatingActionButton(
+        //onPressed: _incrementCounter,
+            onPressed:(){
+        button1(context);
+        },
+
+      tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    );
+  }
+}
+
+
+void button1(BuildContext context){
+  print("Button 1"); //1
+  Navigator.of(context).pushNamed('/screen2'); //2
+}
