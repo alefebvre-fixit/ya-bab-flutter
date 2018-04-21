f6e60043e0cc7756733059d1ff325fb21677c52b
antoine lefebvre <antoine_lefebvre@calypso.com>
2018-04-20 22:45:50 -0700

    add match card

--- lib/match/match-list.dart
+++ lib/match/match-list.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:yabab/match/match.page.dart';
 import 'package:yabab/match/match.model.dart';
@@ -33,13 +34,139 @@ class MatchMakingList extends StatelessWidget {
         if (!snapshot.hasData) return new Text('Loading...');
         return new ListView(
           children: snapshot.data.map((match) {
-            return MatchListTile(match);
+            return MatchCard(match);
           }).toList(),
         );
       },
     );
   }
 }
+
+
+
+class MatchCard extends StatelessWidget {
+  final MatchMaking match;
+
+  MatchCard(this.match);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new ListTile(
+            leading: const Icon(CommunityMaterialIcons.soccer_field),
+            title: new Text('League Name'),
+            subtitle: new Text('@' + 'League Location'),
+          ),
+          new Score(
+            match
+          ),
+          new ButtonTheme.bar(
+            // make buttons use the appropriate styles for cards
+            child: new ButtonBar(
+              children: <Widget>[
+                new FlatButton(
+                  child: const Text('OPEN'),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+class Score extends StatefulWidget {
+  final MatchMaking match;
+
+  Score(this.match);
+
+  @override
+  _ScoreState createState() => new _ScoreState(this.match);
+}
+
+class _ScoreState extends State<Score> {
+  final MatchMaking match;
+
+  _ScoreState(this.match);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        new Row(children: [
+          new Container(
+              child: new Row(
+                children: [
+                  new Container(
+                      margin: const EdgeInsets.only(left: 26.0),
+                      child: new PlayerAvatar(
+                          match, match.team1, match.team1.player1)),
+                  new Container(
+                      margin: const EdgeInsets.only(left: 12.0),
+                      child: new PlayerAvatar(
+                          match, match.team1, match.team1.player2)),
+                ],
+              ))
+        ]),
+        new Expanded(
+            child: new Container(
+              child: new Text(
+                '1' + ':' '2',
+                textAlign: TextAlign.center,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .display1
+                    .copyWith(color: Colors.lightGreen),
+              ),
+            )),
+        new Container(
+            child: new Row(
+              children: [
+                new Container(
+                    margin: const EdgeInsets.only(right: 12.0),
+                    child: new PlayerAvatar(match, match.team2, match.team2.player1)),
+                new Container(
+                    margin: const EdgeInsets.only(right: 26.0),
+                    child: new PlayerAvatar(match, match.team2, match.team2.player2)),
+              ],
+            ))
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class MatchListTile extends StatelessWidget {
   final MatchMaking match;
@@ -86,6 +213,10 @@ class MatchListTile extends StatelessWidget {
     );
   }
 }
+
+
+
+
 
 
 class PlayerAvatar extends StatelessWidget {
