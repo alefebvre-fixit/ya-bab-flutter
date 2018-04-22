de99a873b2819dab7a43fa332ef0f86118364d78
antoine lefebvre <antoine_lefebvre@calypso.com>
2018-04-21 18:16:40 -0700

    fix game list layout

--- lib/match/match.page.dart
+++ lib/match/match.page.dart
@@ -315,107 +315,91 @@ class GameList extends StatelessWidget {
     return new ListView.builder(
       itemCount: values != null ? values.length : 0,
       itemBuilder: (BuildContext context, int index) {
-
-            return new GameListTile(this.match, values[index]);
+        return new GameListTile(this.match, values[index]);
       },
     );
   }
 }
 
 class GameListTile extends StatelessWidget {
-
   final Game game;
   final MatchMaking match;
 
   GameListTile(this.match, this.game);
 
-  _buildStar(BuildContext context, Team team, int score){
-
-    if (score == null){
+  _buildStar(BuildContext context, Team team, int score) {
+    if (score == null) {
       return new Icon(
         Icons.star_border,
         color: Colors.black38,
       );
-    }
-    else if (score >= 10){
+    } else if (score >= 10) {
       return new Icon(
         Icons.star,
         color: team.color,
       );
-    }
-    else {
+    } else {
       return new Icon(
         Icons.star_border,
         color: Colors.black12,
       );
     }
-
-
-
   }
-
 
   @override
   Widget build(BuildContext context) {
-
     return new ListTile(
-      onTap: () => {},
-      leading: _buildStar(context, match.team1, game.scoreTeamA),
-    title: new GameScore(
-        game,
-      ),
-
-      trailing: _buildStar(context, match.team2, game.scoreTeamB)
-    );
+        onTap: () => {},
+        leading: _buildStar(context, match.team1, game.scoreTeamA),
+        title: new GameScore(
+          game,
+        ),
+        trailing: _buildStar(context, match.team2, game.scoreTeamB));
   }
 }
-
 
 class GameScore extends StatelessWidget {
   final Game game;
 
   GameScore(this.game);
 
-  _buildScoreText(BuildContext context, int score){
-
+  _buildScoreText(BuildContext context, int score) {
     Color color = score != null ? Colors.black54 : Colors.black38;
 
     return new Text(
       score != null ? score.toString() : '-',
       textAlign: TextAlign.center,
-      style:
-      Theme.of(context).textTheme.headline.copyWith(color: color),
+      style: Theme.of(context).textTheme.headline.copyWith(color: color),
     );
-
   }
-
 
   @override
   Widget build(BuildContext context) {
-
-
     return new Container(
-      padding: const EdgeInsets.only(left: 80.0, right: 80.0),
-      color: new Color(0X33000000),
-      width: 20.0,
       child: new Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildScoreText(context, game.scoreTeamA),
-          new Text(
-            ':',
-            textAlign: TextAlign.center,
-            style:
-            Theme.of(context).textTheme.headline.copyWith(color: Colors.black54),
+          new Container(
+            width: 40.0,
+            child: _buildScoreText(context, game.scoreTeamA),
           ),
-          _buildScoreText(context, game.scoreTeamB),
-
-
+          new Container(
+              width: 20.0,
+              child: new Text(
+                ':',
+                textAlign: TextAlign.center,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline
+                    .copyWith(color: Colors.black54),
+              )),
+          new Container(
+            width: 40.0,
+            child: _buildScoreText(context, game.scoreTeamB),
+          ),
         ],
       ),
     );
-
-
-
   }
 }
